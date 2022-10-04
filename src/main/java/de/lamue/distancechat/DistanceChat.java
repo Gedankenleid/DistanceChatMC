25b44e07ec695c3caee3c2128d434cfb08a41557
larsmuller <siloks@icloud.com>
2022-10-04 15:27:05 +0200

    Initial commit

---
+++ src/main/java/de/lamue/distancechat/DistanceChat.java
@@ -0,0 +1,17 @@
+package de.lamue.distancechat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DistanceChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
